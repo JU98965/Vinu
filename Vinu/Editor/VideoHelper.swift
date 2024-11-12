7a20837beb3bcd3491ef88219cf9bfc90616bf4c
JU98965 <jjingeo1230@naver.com>
2024-11-12 21:12:52 +0900

    test: Export process facor
    내보내기 진행률 테스트 코드 추가
    
    실행 가능

--- Vinu/Editor/VideoHelper.swift
+++ Vinu/Editor/VideoHelper.swift
@@ -242,14 +242,39 @@ final class VideoHelper {
         // Timerange를 수정한 경우 따로 exporter에 Timerange를 등록하지 않으면 에러가 발생함, 왠지는 모르겠음
         exporter.timeRange = CMTimeRange(start: .zero, duration: composition.duration)
         
-        exporter.exportAsynchronously {
+        Task {
+            try? await print(exporter.estimatedOutputFileLengthInBytes)
+
+            if #available(iOS 18, *) {
+                print("Run at iOS 18")
+                Task {
+                    for await state in exporter.states(updateInterval: 1.0) {
+                        switch state {
+                        case .pending:
+                            print("pending")
+                        case .waiting:
+                            print("waiting")
+                        case .exporting(progress: let progress):
+                            print("exporting", progress.fractionCompleted)
+                        @unknown default:
+                            print("default")
+                        }
+                    }
+                }
+
+                await exporter.export()
+            } else {
+                print("Run at others")
+                await exporter.export()
+            }
+            
             switch exporter.status {
             case .failed:
                 print("Export failed \(exporter.error!)")
             case .completed:
                 if UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(outputURL.path) {
                     UISaveVideoAtPathToSavedPhotosAlbum(outputURL.path, self, nil, nil)
-                    print("completed")
+                    print("끝completed")
                 } else {
                     print("not completed")
                 }
